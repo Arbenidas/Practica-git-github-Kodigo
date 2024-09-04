d44d57b6c69526a36cc7d9b2eed68e8fdef55b5b
Arbenidas <diego00clemente@gmail.com>
2024-09-03 20:29:40 -0600

    Modifique mi clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        String miTexto = "Bienvenido Java Developer";
+        System.out.println("miTexto = " + miTexto);
         System.out.println("Hello world!");
+
     }
 }
